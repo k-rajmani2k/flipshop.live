b1ddc2aa322ab887487b295ad3a505e62f3fd082
Kumar Rajmani Bapat <k.rajmani2k@gmail.com>
2021-02-17 15:01:55 +0530

    d

--- minishop/settings.py
+++ minishop/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY = '3uits7kk-iq2o%wjq2%*6cao)@db46$f2jdd#=*^28=6vdzhq@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['flipshop-live.herokuapp.com']
 
@@ -138,8 +138,8 @@ STATIC_ROOT = 'BASE_DIR' + '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')'''
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 django_heroku.settings(locals())
-MEDIA_URL = 'paramsiddha.wordpress.com/home/downloads/uploads/'
-MEDIA_ROOT = BASE_DIR
+MEDIA_URL = 'https://drive.google.com/drive/u/1/folders/1e9LezHW9aor2zZmBgmPGQ16QLTMMkJeG'
+MEDIA_ROOT = ''
 PAYMENT_API_KEY = 'a1ce78e08fdb4bccfa7b89908a76cc64'
 PAYMENT_API_AUTH_TOKEN = '9c9639f51d59ce753f67578687f0be9a'
 AUTHENTICATION_BACKENDS = (
