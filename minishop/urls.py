e56d0cb2ffedca1504f1f97405b68a1f85a0f4db
Kumar Rajmani Bapat <k.rajmani2k@gmail.com>
2021-02-17 11:29:04 +0530

    a

--- minishop/urls.py
+++ minishop/urls.py
@@ -2,7 +2,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
-from minishop.settings import MEDIA_URL, MEDIA_ROOT,STATIC_URL,STATIC_ROOT
+from minishop.settings import MEDIA_URL,MEDIA_ROOT,STATIC_URL,STATIC_ROOT
 
 urlpatterns = [
     path('admin/', admin.site.urls),
