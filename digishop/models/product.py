a1cf8961fe38242c7d1884479df7b3be7f89b1be
Kumar Rajmani Bapat <k.rajmani2k@gmail.com>
2021-02-19 16:51:17 +0530

    b

--- digishop/models/product.py
+++ digishop/models/product.py
@@ -7,8 +7,8 @@ class Product(models.Model):
     price = models.IntegerField(default=0)
     active = models.BooleanField(default=0)
     discount = models.IntegerField(default=0)
-    file = models.FileField(upload_to='staticfiles', null=True,blank=True)
-    thumbnail = models.ImageField('minishop/uploads/thumbnail')
+    file = models.FileField(null=True,blank=True)
+    thumbnail = models.ImageField()
     link = models.CharField(null=True,blank=True,max_length=500)
     fileSize = models.CharField(null=True,max_length=500)
 
@@ -18,4 +18,4 @@ class Product(models.Model):
 
 class ProductImages(models.Model):
     product = models.ForeignKey(Product, default=None, on_delete=models.CASCADE)
-    image = models.ImageField(upload_to='uploads/images', blank=True)
+    image = models.ImageField(blank=True)
