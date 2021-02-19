5ce55d94bef69f6103e7df360548d72d280c7b9e
Kumar Rajmani Bapat <k.rajmani2k@gmail.com>
2021-02-19 17:49:42 +0530

    b

--- minishop/settings.py
+++ minishop/settings.py
@@ -133,12 +133,12 @@ STATIC_URL = '/static/paramsiddha/hub/123/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )'''
-STATIC_URL = '/static/paramsiddha/hub/123/'
+STATIC_URL = '/static/'
 STATIC_ROOT =os.path.join(BASE_DIR,'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 django_heroku.settings(locals())
-MEDIA_URL = '/download/paidproduct/<int:product_id>/'
-MEDIA_ROOT = BASE_DIR
+MEDIA_URL = 'https://drive.google.com/drive/folders/1e9LezHW9aor2zZmBgmPGQ16QLTMMkJeG/'
+MEDIA_ROOT = 'https://drive.google.com/drive/folders/1e9LezHW9aor2zZmBgmPGQ16QLTMMkJeG'
 PAYMENT_API_KEY = 'a1ce78e08fdb4bccfa7b89908a76cc64'
 PAYMENT_API_AUTH_TOKEN = '9c9639f51d59ce753f67578687f0be9a'
 AUTHENTICATION_BACKENDS = (
