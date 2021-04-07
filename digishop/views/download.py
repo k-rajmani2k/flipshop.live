1578f4ab126aeb237d3de95dccda0ed96da7041d
Kumar Rajmani Bapat <k.rajmani2k@gmail.com>
2021-04-07 20:25:03 +0530

    change their

--- digishop/views/download.py
+++ digishop/views/download.py
@@ -29,5 +29,5 @@ def downloadPaidProduct(request, product_id):
         return redirect('index')
 
 def proimages(request, product_id):
-    product = Product.objects.get(id=product_id)
-    return redirect(product.thumbnail)
+
+    return redirect(Product.thumbnail)
