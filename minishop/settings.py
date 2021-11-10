9147db18f966655a36ea69b7224852142746906b
Kumar Rajmani Bapat <k.rajmani2k@gmail.com>
2021-11-10 22:36:04 +0530

    testing key updated changes to exisiting one

--- minishop/settings.py
+++ minishop/settings.py
@@ -139,8 +139,8 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 django_heroku.settings(locals())
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
-PAYMENT_API_KEY = 'a1ce78e08fdb4bccfa7b89908a76cc64'
-PAYMENT_API_AUTH_TOKEN = '9c9639f51d59ce753f67578687f0be9a'
+PAYMENT_API_KEY = 'test_fa9cfd5d94b90e5f4a0c8e041fb'
+PAYMENT_API_AUTH_TOKEN = 'test_716154a9ff25431e4e0b3905532'
 AUTHENTICATION_BACKENDS = (
     ('django.contrib.auth.backends.ModelBackend'),
 )
